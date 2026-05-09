d2447d6b4d4d02709268ae3d37d93deb0a7e9a63
rencrash97 <renzo_larenaspiatta@hotmail.com>
2026-05-09 18:47:55 -0300

    feature (#4):Finalizado validación de Login y redirecionamiento a pagina de usuario

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -1,7 +1,8 @@
-//validación de formularios y localStorage
+//validación ingreso
 
-const alertError = document.getElementById("errorDatosRegistro");
-const registrar = document.getElementById("botonRegistro");
+const alertError = document.getElementById("errorDatosIngreso");
+const ingresar = document.getElementById("botonIngreso");
+
 const renderError = (text) => {
     alertError.textContent = text;
     setTimeout(() => {
@@ -9,60 +10,29 @@ const renderError = (text) => {
     }, 3000);
 };
 
-registrar.addEventListener("click", () => {
+ingresar.addEventListener("click", () => {
 
-    const id = window.crypto.randomUUID();
-    const nombre = document.getElementById("nombre").value;
-    const correo = document.getElementById("correo").value;
-    const nacimiento = document.getElementById("nacimiento").value;
-    const contra = document.getElementById("contra").value;
-    const contraPrueba = document.getElementById("contraPrueba").value;
-    var validar = false;
-    alertError.textContent = null;
+    const usuarioStorage = JSON.parse(localStorage.getItem("usuario"));
 
-    function validarContra(contra) {
-        const simbolos = /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
-        return simbolos.test(contra);
+    const correoLogin = document.getElementById("correoLogin").value;
+    const contraLogin = document.getElementById("contraLogin").value;
+
+    const correoUsuario = usuarioStorage.correo;
+    const contraUsuario = usuarioStorage.contra;
+
+    if(correoLogin !== correoUsuario) {
+        document.getElementById("contraLogin").value = "";
+        return renderError("El correo electrónico ingresado no es correcto, intente nuevamente");
+        
     }
 
-    const usuario = {
-        id,
-        nombre,
-        correo,
-        nacimiento,
-        contra
-    };
-
-    for (const i in usuario) {
-        if (!usuario[i]) {
-            return renderError("Asegúrese de llenar todos los campos");
-        }
+    if(contraLogin !== contraUsuario) {
+        document.getElementById("contraLogin").value = "";
+        return renderError("La contraseña ingresada no es correcta, intente nuevamente");
     }
 
-    if (!correo.trim().includes("@")) {
-        return renderError("Asegúrese de que el correo esté bien escrito");
+    if(correoLogin === correoUsuario && contraLogin === contraUsuario) {
+        window.location.href = "./perfil.html"
     }
 
-    if (!validarContra(contra)) {
-        return renderError("Asegúrese que la contraseña contenga al menos una Mayúscula y un símbolo");
-    }
-
-    if (contra !== contraPrueba) {
-        return renderError("Asegúrese que las contraseñas coincidan");
-    }
-
-    validar = true;
-
-    if (validar) {
-        localStorage.setItem("usuario", JSON.stringify(usuario));
-        document.getElementById("registrado").textContent = "Registrado exitosamente! Redireccionando al ingreso...";
-        setTimeout(() => {
-            window.location.href = "./login.html";
-        }, 3000);
-    }
 });
-
-//validación ingreso
-
-
-
